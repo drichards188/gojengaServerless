00f6557b9cd8dae2c54ed122f5c37c973dff531c
drichards188 <github@cxoprotect.com>
2024-02-27 16:37:56 -0700

    get_all_diverse_symbols working

--- risk/get_all_diverse_symbols/lambda_function.py
+++ risk/get_all_diverse_symbols/lambda_function.py
@@ -5,11 +5,6 @@ import os
 
 
 def lambda_handler(event, context):
-
-    symbol = event["pathParameters"]["symbol"]
-
-    print(f"symbol is {symbol}")
-
     # Connection configuration
     config = {
         'user': os.environ['DB_USER'],
@@ -19,21 +14,18 @@ def lambda_handler(event, context):
         'raise_on_warnings': True
     }
 
-    # Connect to the database
     try:
         connection = mysql.connector.connect(**config)
         cursor = connection.cursor()
         table_name = "sharpe_calc"
 
-        stmt = f"SELECT sharpe_ratio FROM {table_name} WHERE symbol = '{symbol.upper()}';"
+        stmt = f"SELECT symbol FROM {table_name};"
         cursor.execute(stmt)
         results = cursor.fetchall()
-
-
-        pretty_results = {}
+        pretty_results = []
 
         for item in results:
-            pretty_results["sharpe_ratio"] = item[0]
+            pretty_results.append(item[0])
 
         print(f"results are {pretty_results}")
 
@@ -56,8 +48,8 @@ def lambda_handler(event, context):
             'statusCode': 500,
             'headers': {
                 "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Headers": 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token, Is-Test',
-                "Access-Control-Allow-Methods": "*"
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
             },
             'body': "Database connection failed"
         }
--- risk/get_all_sharpe_symbols/lambda_function.py
+++ risk/get_all_sharpe_symbols/lambda_function.py
@@ -1,9 +1,10 @@
+import json
+
 import mysql.connector
 import os
 
 
 def lambda_handler(event, context):
-
     # Connection configuration
     config = {
         'user': os.environ['DB_USER'],
@@ -16,7 +17,7 @@ def lambda_handler(event, context):
     try:
         connection = mysql.connector.connect(**config)
         cursor = connection.cursor()
-        table_name = "sharpe_calc"
+        table_name = "spdr_calculations"
 
         stmt = f"SELECT symbol FROM {table_name};"
         cursor.execute(stmt)
@@ -34,11 +35,21 @@ def lambda_handler(event, context):
 
         return {
             'statusCode': 200,
-            'body': pretty_results
+            'headers': {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token, Is-Test',
+                "Access-Control-Allow-Methods": "*"
+            },
+            'body': json.dumps(pretty_results)
         }
     except mysql.connector.Error as e:
         print(f"Database connection failed: {e}")
         return {
             'statusCode': 500,
+            'headers': {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+            },
             'body': "Database connection failed"
         }
